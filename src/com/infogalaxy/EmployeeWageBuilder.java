758b3b4802ecb5cf1fa89acffa6529dc99972ff6
Ashwinijava1pm <ashwinigirase12@gmail.com>
2022-03-08 12:31:33 +0530

    MODIFIED: Employee Daily Wage Calculation

--- src/com/infogalaxy/EmployeeWageBuilder.java
+++ src/com/infogalaxy/EmployeeWageBuilder.java
@@ -6,23 +6,34 @@ public class EmployeeWageBuilder {
     public static void main(String args[]){
         System.out.println("Welcome Employee Wage Calculation:");
         Random r = new Random();
+
         //UC-2 Employee Daily Wages Calculation
         final int WAGE_PER_HOUR = 20;
-        final int DAY_PER_HOUR = 8;
+        final int FULL_TIME_DAY_HOUR = 8;
+        final int PART_TIME_HOUR = 4;
         int total_wage = 0;
-        int empAttendence = r.nextInt()%2;
-        System.out.println(empAttendence);
+        int employeeAttendance = r.nextInt()%2;
+        System.out.println(employeeAttendance);
         //UC-1 Employee Attendance
-        if(empAttendence ==   0){
+        if(employeeAttendance ==   0){
             System.out.println("Employe is Absent:");
             System.out.println("Employe wages="+total_wage);
         }
-        else if(empAttendence == 1) {
-
+        //UC-3-Employee Part Time Wage Calculation
+        else if(employeeAttendance == 1) {
+            int emp_Type;
             System.out.println("Employee is Present:");
+            employeeAttendance = r.nextInt()%2;
+            System.out.println("Emp_Type="+employeeAttendance);
+            if(employeeAttendance == 1) {
+                total_wage = WAGE_PER_HOUR * FULL_TIME_DAY_HOUR;
+                System.out.println("Employe Full Time Wages=" + total_wage);
+            }
+            else if(employeeAttendance == 0){
+                total_wage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                System.out.println("Employee Part Time Wage="+total_wage);
+            }
 
-            total_wage = WAGE_PER_HOUR * DAY_PER_HOUR;
-            System.out.println("Employe Wages="+total_wage);
             
         }
         else{
